3c248e04bc289561ef10b0597eb6f650935ac302
Hashir <hashir.logic@gmail.com>
2025-08-06 14:47:56 +0500

    Initial commit

--- 10-gpa-calculator/App.jsx
+++ 10-gpa-calculator/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-// import './App.css';
+import './App.css';
 
 function GPACalculator() {
   const [subjects, setSubjects] = useState([{ name: '', marks: '', creditHours: '' }]);
