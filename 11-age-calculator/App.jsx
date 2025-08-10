c0bd690c91baccc4dde0a47ca9d6d8f257721679
Hashir <hashir.logic@gmail.com>
2025-08-10 13:39:20 +0500

    Initial commit

--- 11-age-calculator/App.jsx
+++ 11-age-calculator/App.jsx
@@ -1,11 +1,18 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [date, setDate] = useState()
-  const [month, setMonth] = useState()
-  const [year, setYear] = useState()
+  const [date, setDate] = useState("")
+  const [month, setMonth] = useState("")
+  const [year, setYear] = useState("")
+  const [dateCal, setDateCal] = useState("")
+  const [monthCal, setMonthCal] = useState("")
+  const [yearCal, setYearCal] = useState("")
   const [age, setAge] = useState(false)
+
+  useEffect(() => {
+    setAge(false)
+  }, [date, month, year])
 
   const calculate = () => {
     let obj = new Date(Date.now())
@@ -13,29 +20,66 @@ function App() {
     let m = obj.getMonth()
     let d = obj.getDate()
 
-    let cy = y - year
-    let cm = m - month
-    let cd = d - date
+    let cy = parseInt(y) - year
+    let cm = parseInt(m) - month
+    let cd
+    let h
+    if (m === 2) {
+      h = 28 - parseInt(date)
+      cd = h + parseInt(d)
+      if (cd > 30) {
+        cm += 1
+        cd = cd - 30
+      }
+    }
 
-    setYear(cy)
-    setMonth(cm)
-    setDate(cd)
+    else if (m % 2 === 1) {
+      h = 31 - parseInt(date)
+      cd = h + parseInt(d)
+      if (cd > 30) {
+        cm += 1
+        cd = cd - 30
+      }
+    }
+
+    else {
+      h = 30 - parseInt(date)
+      cd = h + parseInt(d)
+      if (cd > 30) {
+        cm += 1
+        cd = cd - 30
+      }
+    }
+
+    setYearCal(cy)
+    setMonthCal(cm)
+    setDateCal(cd)
 
     setAge(true)
-    
+
   }
 
   return (
     <div className="container">
-      <input type="number" value={date} onChange={(e) => setDate(e.target.value)} />
+      <label htmlFor="date">
+        Date:
+        <input id='date' type="number" value={date} onChange={(e) => setDate(e.target.value)} />
+      </label>
 
-      <input type="number" value={month} onChange={(e) => setMonth(e.target.value)} />
+      <label htmlFor="month">
+        Month:
+        <input id='month' type="number" value={month} onChange={(e) => setMonth(e.target.value)} />
+      </label>
 
-      <input type="number" value={year} onChange={(e) => setYear(e.target.value)} />
 
-      <button onClick={calculate}>Calculate</button>
+      <label htmlFor="year">
+        Year:
+        <input id='year' type="number" value={year} onChange={(e) => setYear(e.target.value)} />
+      </label>
 
-      {age && <p>your age is {year} years {month} months and {date} days</p>}
+      <button onClick={calculate} disabled={age}>Calculate</button>
+
+      {age && <p>your age is {yearCal} years {monthCal} months and {dateCal} days</p>}
     </div>
   )
 }
