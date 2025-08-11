da7076ec460c8361b7069d5c96bcd856c9c4315d
Hashir <hashir.logic@gmail.com>
2025-08-11 15:50:32 +0500

    Initial commit

--- 11-age-calculator/App.jsx
+++ 11-age-calculator/App.jsx
@@ -5,81 +5,111 @@ function App() {
   const [date, setDate] = useState("")
   const [month, setMonth] = useState("")
   const [year, setYear] = useState("")
-  const [dateCal, setDateCal] = useState("")
-  const [monthCal, setMonthCal] = useState("")
-  const [yearCal, setYearCal] = useState("")
-  const [age, setAge] = useState(false)
+  const [calculatedDate, setCalculatedDate] = useState("")
+  const [calculatedMonth, setCalculatedMonth] = useState("")
+  const [calculatedYear, setCalculatedYear] = useState("")
+  const [isCalculated, setIsCalculated] = useState(false)
+
+  let obj = new Date(Date.now())
+  let getFullYear = obj.getFullYear()
+  let getMonth = obj.getMonth()
+  let getDate = obj.getDate()
 
   useEffect(() => {
-    setAge(false)
+    setIsCalculated(false)
   }, [date, month, year])
 
-  const calculate = () => {
-    let obj = new Date(Date.now())
-    let y = obj.getFullYear()
-    let m = obj.getMonth()
-    let d = obj.getDate()
+  const handleDate = (value) => {
+    const num = Number(value)
 
-    let cy = parseInt(y) - year
-    let cm = parseInt(m) - month
-    let cd
-    let h
-    if (m === 2) {
-      h = 28 - parseInt(date)
-      cd = h + parseInt(d)
-      if (cd > 30) {
-        cm += 1
-        cd = cd - 30
+    if (month === 2) {
+      if (num > 0 && num <= 28) setDate(num)
+    }
+    else if (month % 2 === 1) {
+      if (num > 0 && num <= 31) setDate(num)
+    }
+    else {
+      if (num > 0 && num <= 30) setDate(num)
+    }
+  }
+
+  const calculate = () => {
+
+    let calculateYears = parseInt(getFullYear) - year
+    let calculateMonths = parseInt(getMonth) - month
+    let calculateDays
+    let temp
+    if (getMonth === 2) {
+      temp = 28 - parseInt(date)
+      calculateDays = temp + parseInt(getDate)
+      if (calculateDays > 30) {
+        calculateMonths += temp
+        calculateDays = calculateDays - 30
       }
     }
 
-    else if (m % 2 === 1) {
-      h = 31 - parseInt(date)
-      cd = h + parseInt(d)
-      if (cd > 30) {
-        cm += 1
-        cd = cd - 30
+    else if (getMonth % 2 === 1) {
+      temp = 31 - parseInt(date)
+      calculateDays = temp + parseInt(getDate)
+      if (calculateDays > 30) {
+        calculateMonths += 1
+        calculateDays = calculateDays - 30
       }
     }
 
     else {
-      h = 30 - parseInt(date)
-      cd = h + parseInt(d)
-      if (cd > 30) {
-        cm += 1
-        cd = cd - 30
+      temp = 30 - parseInt(date)
+      calculateDays = temp + parseInt(getDate)
+      if (calculateDays > 30) {
+        calculateMonths += 1
+        calculateMonths = calculateDays - 30
       }
     }
 
-    setYearCal(cy)
-    setMonthCal(cm)
-    setDateCal(cd)
+    setCalculatedYear(calculateYears)
+    setCalculatedMonth(calculateMonths)
+    setCalculatedDate(calculateDays)
 
-    setAge(true)
+    setIsCalculated(true)
 
   }
 
   return (
     <div className="container">
-      <label htmlFor="date">
-        Date:
-        <input id='date' type="number" value={date} onChange={(e) => setDate(e.target.value)} />
+
+      <label htmlFor="year">
+        Year:
+        <input
+          id='year'
+          type="number"
+          value={year}
+          onChange={(e) => (
+            (e.target.value <= getFullYear) && setYear(e.target.value))} />
       </label>
 
       <label htmlFor="month">
         Month:
-        <input id='month' type="number" value={month} onChange={(e) => setMonth(e.target.value)} />
+        <input
+          id="month"
+          type="number"
+          value={month}
+          onChange={(e) => (
+            (e.target.value <= 12) && setMonth(e.target.value))}/>
       </label>
 
-
-      <label htmlFor="year">
-        Year:
-        <input id='year' type="number" value={year} onChange={(e) => setYear(e.target.value)} />
+      <label htmlFor="date">
+        Date:
+        <input
+          id="date"
+          type="number"
+          value={date}
+          onChange={(e) => handleDate(e.target.value)}
+        />
       </label>
 
-      <button onClick={calculate} disabled={age}>Calculate</button>
+      <button onClick={calculate} disabled={isCalculated}>Calculate</button>
 
-      {age && <p>your age is {yearCal} years {monthCal} months and {dateCal} days</p>}
+      {isCalculated && <p>your age is {calculatedYear} years {calculatedMonth} months and {calculatedDate} days</p>}
     </div>
   )
 }
