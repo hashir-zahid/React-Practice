4b66bf3b6275cefee267c51da5319042e374288d
Hashir <hashir.logic@gmail.com>
2025-08-14 14:42:46 +0500

    Initial commit

--- 12-password-generator/App.jsx
+++ 12-password-generator/App.jsx
@@ -5,30 +5,59 @@ import './App.css'
 
 function App() {
   const [password, setPassword] = useState("")
+  const [size, setSize] = useState(8)
+  const [isNumber, setIsNumber] = useState(false)
+  const [isSpecial, setIsSpecial] = useState(false)
 
   const handleGenerate = () => {
+    let pass = ""
+    let characters = "abcdefghijklmnopqrstuvwxyz"
+    let specialCharacters = "!@#$%^&*?~"
+    let numbers = "1234567890"
 
+    for (let i = 0; i < size; i++) {
+      let randomIndex = Math.floor(Math.random() * characters.length);
+      pass += characters[randomIndex];
+    }
 
-  }
+    setPassword(pass)
+  
+}
 
-  return (
-    <div className='container'>
-      <div className="input">
-        <input type="text" className='inp' maxlength="20"/>
-        <button className='btn'>Copy</button>
-      </div>
-
-      <input type="range" min="8" max="20" /><br />
-
-      <input type="checkbox" />
-      <label htmlFor="">Numbers</label><br />
-
-      <input type="checkbox" />
-      <label htmlFor="">Special Characters</label><br />
-
-      <button onClick={handleGenerate}>Generate</button>
+return (
+  <div className='container'>
+    <div className="input">
+      <input type="text" className='inp' maxLength="20" value={password} disabled/>
+      <button className='btn'>Copy</button>
     </div>
-  )
+
+    <input
+      type="range"
+      min="8"
+      max="20"
+      value={size}
+      onChange={(e) => setSize(e.target.value)}
+    />
+    <label>{size}</label>
+    <br />
+
+    <input type="checkbox"
+      checked={isNumber}
+      onChange={(e) => setIsNumber(e.target.value)}
+    />
+    <label htmlFor="">Numbers</label>
+    <br />
+
+    <input type="checkbox"
+      checked={isSpecial}
+      onChange={(e) => setIsSpecial(e.target.value)}
+    />
+    <label htmlFor="">Special Characters</label>
+    <br />
+
+    <button onClick={handleGenerate}>Generate</button>
+  </div>
+)
 }
 
 export default App
