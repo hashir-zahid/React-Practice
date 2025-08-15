327508ad860aedd4b3cdc2d16ba40b1ac7f7ab6e
Hashir <hashir.logic@gmail.com>
2025-08-15 15:15:20 +0500

    Initial commit

--- 12-password-generator/App.jsx
+++ 12-password-generator/App.jsx
@@ -16,48 +16,77 @@ function App() {
     let numbers = "1234567890"
 
     for (let i = 0; i < size; i++) {
-      let randomIndex = Math.floor(Math.random() * characters.length);
-      pass += characters[randomIndex];
+      if (isNumber && isSpecial) {
+        characters = characters + numbers + specialCharacters
+        let randomIndex = Math.floor(Math.random() * characters.length);
+        pass += characters[randomIndex];
+      }
+      else if (isSpecial) {
+        characters = characters + specialCharacters
+        let randomIndex = Math.floor(Math.random() * characters.length);
+        pass += characters[randomIndex];
+      }
+      else if (isNumber) {
+        characters = characters + numbers
+        let randomIndex = Math.floor(Math.random() * characters.length);
+        pass += characters[randomIndex];
+      }
+      else {
+        let randomIndex = Math.floor(Math.random() * characters.length);
+        pass += characters[randomIndex];
+      }
     }
 
     setPassword(pass)
-  
-}
+  }
 
-return (
-  <div className='container'>
-    <div className="input">
-      <input type="text" className='inp' maxLength="20" value={password} disabled/>
-      <button className='btn'>Copy</button>
+  const handleCopy = () => {
+    navigator.clipboard.writeText(password);
+
+    const toast = document.getElementsByClassName("toast");
+    toast.classList.add("show");
+
+    setTimeout(() => {
+      toast.classList.remove("show");
+    }, 2000);
+  };
+
+
+  return (
+    <div className='container'>
+      <div className='toast'>Password is copied!</div>
+      <div className="input">
+        <input type="text" className='inp' maxLength="20" value={password} disabled />
+        <button className='btn' onClick={handleCopy}>Copy</button>
+      </div>
+
+      <input
+        type="range"
+        min="8"
+        max="20"
+        value={size}
+        onChange={(e) => setSize(e.target.value)}
+      />
+      <label>{size}</label>
+      <br />
+
+      <input type="checkbox"
+        checked={isNumber}
+        onChange={() => setIsNumber(!isNumber)}
+      />
+      <label htmlFor="">Numbers</label>
+      <br />
+
+      <input type="checkbox"
+        checked={isSpecial}
+        onChange={() => setIsSpecial(!isSpecial)}
+      />
+      <label htmlFor="">Special Characters</label>
+      <br />
+
+      <button onClick={handleGenerate}>Generate</button>
     </div>
-
-    <input
-      type="range"
-      min="8"
-      max="20"
-      value={size}
-      onChange={(e) => setSize(e.target.value)}
-    />
-    <label>{size}</label>
-    <br />
-
-    <input type="checkbox"
-      checked={isNumber}
-      onChange={(e) => setIsNumber(e.target.value)}
-    />
-    <label htmlFor="">Numbers</label>
-    <br />
-
-    <input type="checkbox"
-      checked={isSpecial}
-      onChange={(e) => setIsSpecial(e.target.value)}
-    />
-    <label htmlFor="">Special Characters</label>
-    <br />
-
-    <button onClick={handleGenerate}>Generate</button>
-  </div>
-)
+  )
 }
 
 export default App
