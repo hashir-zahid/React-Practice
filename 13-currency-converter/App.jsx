5a689e296a52bdae96769d31e3778e593a4d64b0
Hashir <hashir.logic@gmail.com>
2025-10-05 14:57:30 +0500

    Initial commit

--- 13-currency-converter/App.jsx
+++ 13-currency-converter/App.jsx
@@ -1,13 +1,21 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useCurrency } from './useCurrency'
 import './App.css'
 
 function App() {
+  const [currency, setCurrency] = useState("usd");
+  const { data, loading } = useCurrency(currency);
 
   return (
     <>
       <h1>currency converter</h1>
+      <input
+        type="text"
+        value={currency}
+        onChange={(e) => setCurrency(e.target.value.toLowerCase())}
+      />
+
+      {loading ? <p>Loading...</p> : <li>PKR: {data?.pkr}</li>}
     </>
   )
 }
