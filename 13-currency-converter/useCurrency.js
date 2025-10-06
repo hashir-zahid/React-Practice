ff9cbec79bb31ebf5baa6a9e239b3bed9fe761c2
Hashir <hashir.logic@gmail.com>
2025-10-06 15:01:49 +0500

    Initial commit

--- 13-currency-converter/useCurrency.js
+++ 13-currency-converter/useCurrency.js
@@ -1,16 +1,30 @@
-import { useState, useEffect } from 'react'
-import React from 'react'
+import { useState, useEffect } from "react";
 
-function useCurrency(currency) {
-  const [data, setData] = useState({})
+export function useCurrency(currency) {
+  const [data, setData] = useState({});
+  const [loading, setLoading] = useState(true);
 
-  useEffect(() => 
-  ('https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json')
-  .then((res) => res.json())
-  .then((res) => setData(res[currency]))
-  )
+  useEffect(() => {
+    if (!currency) return;
 
-  return data
+    async function dataFetch() {
+      try {
+        setLoading(true);
+        const res = await fetch(
+          https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json
+        );
+        const result = await res.json();
+        setData(result[currency]); 
+        console.log(result[currency]);
+      } catch (e) {
+        console.error("Error fetching rates:", e);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    dataFetch();
+  }, [currency]);
+
+  return { data, loading };
 }
-
-export default useCurrency
