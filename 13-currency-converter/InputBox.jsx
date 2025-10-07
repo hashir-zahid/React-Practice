ff8b2c4e9dfa46450403d2bf4028a32bee669627
Hashir <hashir.logic@gmail.com>
2025-10-07 14:55:37 +0500

    Initial commit

---
+++ 13-currency-converter/InputBox.jsx
@@ -0,0 +1,51 @@
+import React, { useId } from 'react'
+
+function InputBox(
+    label,
+    amount,
+    onAmountChange,
+    onCurrencyChange,
+    currencyOptions = [],
+    selectCurrency,
+    amountDisable,
+    currencyDisable
+) {
+    const amountId = useId()
+
+    return (
+        <div className='container'>
+            <div className="box1">
+                <label htmlFor={amountId}>
+                    {label}
+                </label>
+                <input
+                    type="number"
+                    id={amountId}
+                    placeholder='Amount'
+                    value={amount}
+                    onChange={(e) => onAmountChange && onAmountChange(Number(e.target.value))}
+                    disabled={currencyDisable}
+                />
+            </div>
+            <div className="box2">
+                <p>Currency Type</p>
+                <select
+                    value={amount}
+                    onChange={(e) => onAmountChange && onAmountChange(Number(e.target.value))}
+                    disabled={currencyDisable}
+                >
+
+                    {currencyOptions.map((currency) => (
+                        <option key={currency} value={currency}>
+                            {currency}
+                        </option>
+                    ))}
+
+
+                </select>
+            </div>
+        </div>
+    )
+}
+
+export default InputBox
