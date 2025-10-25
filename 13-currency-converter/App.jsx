0eb1f0ceee8687f6939fd2335eb6018fc05fbb05
Hashir <hashir.logic@gmail.com>
2025-10-25 14:31:32 +0500

    Initial commit

--- 13-currency-converter/App.jsx
+++ 13-currency-converter/App.jsx
@@ -1,22 +1,65 @@
 import { useState } from 'react'
 import { useCurrency } from './useCurrency'
+import InputBox from './InputBox'
 import './App.css'
 
 function App() {
-  const [currency, setCurrency] = useState("usd");
-  const { data, loading } = useCurrency(currency);
+  const [from, setFrom] = useState("usd")
+  const [to, setTo] = useState("pkr")
+  const [amount, setAmount] = useState(0)
+  const [convertedAmount, setConvertedAmont] = useState(0)
+
+  const currencyInfo = useCurrency(from)
+
+  const options = Object.keys(currencyInfo)
 
   return (
-    <>
-      <h1>currency converter</h1>
-      <input
-        type="text"
-        value={currency}
-        onChange={(e) => setCurrency(e.target.value.toLowerCase())}
-      />
+    <div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          convert()
+        }}
+      >
 
-      {loading ? <p>Loading...</p> : <li>PKR: {data?.pkr}</li>}
-    </>
+        <div>
+          <InputBox
+            label="From"
+            amount={amount}
+            currencyOptions={options}
+            onCurrencyChange={(currency) => setAmount(amount)}
+            selectCurrency={from}
+            onAmountChange={(amount) => setAmount(amount)}
+          />
+        </div>
+
+        <div>
+          <button
+            type="button"
+            onClick={swap}
+          >
+            swap
+          </button>
+        </div>
+
+        <div>
+          <InputBox
+            label="To"
+            amount={convertedAmount}
+            currencyOptions={options}
+            onCurrencyChange={(currency) => setTo(currency)}
+            selectCurrency={from}
+            amountDisable
+          />
+        </div>
+
+        <button type="submit">
+          Convert {from.toUpperCase()} to {to.toUpperCase()}
+        </button>
+
+      </form>
+
+    </div>
   )
 }
 
